d5558cb05cd620fa2dc3a43102157be41114ab8d
LuluTchab <l.chaboudez@gmail.com>
2018-07-10 17:30:45 +0200

    Remove files after parsing

--- src/parse-all.sh
+++ src/parse-all.sh
@@ -3,6 +3,8 @@
 
 ZIP_DIR="/srv/${WP_ENV}/jahia2wp/data/exports/"
 
+EXPORT_DIR="/srv/${WP_ENV}/jahia2wp/data/jahia-data/"
+
 
 for zipFile in `ls ${ZIP_DIR} | egrep ".zip$"`
 do
@@ -10,7 +12,11 @@ do
     siteName=`echo ${zipFile} | awk -F"_" '{print $1}'`
 
     echo "Parsing ${siteName} ..."
-    python jahia2wp.py parse ${siteName}
+    python jahia2wp.py parse ${siteName} --quiet
+    echo "Removing extracted files..."
+    rm -rf "${EXPORT_DIR}/${siteName}"
+
+
 
 done
 
