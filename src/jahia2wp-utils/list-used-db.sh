ef4a12630890ed10e5753937c1db38dde9289ac8
LuluTchab <l.chaboudez@gmail.com>
2018-07-18 10:33:49 +0200

    List used DB by parsing 'wp-config.php' files in folder

---
+++ src/jahia2wp-utils/list-used-db.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+
+# Check parameters
+
+if [ "$1" == "" ]
+then
+    echo "Root path missing"
+    exit 1
+fi
+
+ROOT_PATH=$1
+
+for wpConfig in `find ${ROOT_PATH} -name "wp-config.php"`
+do
+    siteFolder=`echo ${wpConfig} | awk -F"/" '{print $(NF-1)}'`
+    dbName=`grep "'DB_NAME'" ${wpConfig} | awk -F, '{print $2}' | awk -F\' '{print $2}'`
+
+    echo "${siteFolder},${dbName}"
+
+done
